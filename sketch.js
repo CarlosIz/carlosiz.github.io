efe51fc208569a4e35fdd7d0e266315ac6c4f9bd
CarlosIz <CarlosIz@users.noreply.github.com>
2019-11-04 11:31:03 -0800

    version 4.1

--- sketch.js
+++ sketch.js
@@ -1,14 +1,7 @@
-//CSMA 101 
-//Introduction to Programming
-//Semester 1
-//Professor Theohar
-//Carlos Izaguirre
-//carlos.izaguirre@woodbury.edu
-
 function setup() {
-  
   createCanvas(400, 400);
   noFill();
+
 }
 
 function draw() {
@@ -17,25 +10,6 @@ function draw() {
 }
 
 function mousePressed() {
-
-// Brush 3
-  
-  
-// let location = random(15);  
-// let size = random(40);
-//   strokeWeight(2)
-//   stroke(0);
-//   fill(random(255));
-//   rect(mouseX - location, mouseY - location, size, size);
-    
-// Brush 2
-  
-// fill(0, 40, 0);
-// noStroke();
-// square(mouseX, mouseY, 25);
-   
-//   Brush 1
-  
   fill(255);
   noStroke();
   ellipse(mouseX, mouseY, 30, 25);
@@ -43,29 +17,8 @@ function mousePressed() {
   
 }
 function mouseDragged() {
-
-// Brush 3
-  
-// let location = random(40);  
-// let size = random(40);
-//   strokeWeight(2)
-//   stroke(0);
-//   fill(random(255));
-//   rect(mouseX - location, mouseY - location, size, size); 
-  
-  
-//   Brush 2
-  
-// fill(0, 40, 0);
-// noStroke();
-// square(mouseX, mouseY, 25);
-  
-  
-// Brush 1  
-  
   fill(255, 0, 0);
   noStroke();
   ellipse(mouseX, mouseY, 30, 25);
-  
   
 }

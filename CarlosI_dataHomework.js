d7962a1a4d08689334de5a5aa097cf93f72dcb9a
CarlosIz <CarlosIz@users.noreply.github.com>
2019-11-06 10:14:29 -0800

    version 5.1

--- CarlosI_dataHomework.js
+++ CarlosI_dataHomework.js
@@ -21,7 +21,7 @@ function setup() {
   noStroke();
 
   //html stuff
-  canvas.position(400, 200);
+  canvas.position(350, 200);
   canvas.class("areaC");
 
   for (i = 0; i < gameTable.getRowCount(); i++) {
@@ -30,7 +30,7 @@ function setup() {
     activeP[i] = gameTable.getNum(i, 2);
     category[i] = gameTable.getString(i, 3);
     time[i] = gameTable.getString(i, 4);
-    sz[i] = map(activeP[i]/3, 1, 10, 10, 80);
+    sz[i] = map(activeP[i] / 3, 1, 10, 10, 80);
 
   }
 }
@@ -39,14 +39,14 @@ function draw() {
 
   background(0, 0, 36);
 
-    for (var i = 0; i < gameTable.getRowCount(); i++) {
+  for (var i = 0; i < gameTable.getRowCount(); i++) {
 
 
     //bar size and color
     fill(hueVal[i], 80, 80);
     push();
     translate(-150, 0);
-    rect(197.5 * (i+1), height, 100, -sz[i]);
+    rect(197.5 * (i + 1), height, 100, -sz[i]);
     pop();
 
     //info in bar
@@ -56,27 +56,26 @@ function draw() {
     translate(-110, 0);
     push();
     translate(0, height);
-    text(names[i], 190 * (i+1), -sz[i] + 30);
-    text(activeP[i], 190 * (i+1), -sz[i] + 70);
-    text(category[i], 190 * (i+1), -sz[i ]+ 90);
-    text(time[i], 190 * (i+1), -sz[i] + 110);
+    text(names[i], 190 * (i + 1), -sz[i] + 30);
+    text(activeP[i], 190 * (i + 1), -sz[i] + 70);
+    text(category[i], 190 * (i + 1), -sz[i] + 90);
+    text(time[i], 190 * (i + 1), -sz[i] + 110);
     pop();
     pop();
-    }
+  }
 
-if(legA == true) {
-  fill(0);
-  textSize(20);
-  text("Press SHIFT for legend", 760, 50);
-}
-if(keyIsDown(SHIFT)) {
-  //legend
-  fill(0);
-  textSize(24);
-  text("1. Game Name\n2. Active Players\n3. Category\n4. Run Time", 800, 50);
-  legA = false;
-    }
-    else {
-        legA = true;
+  if (legA == true) {
+    fill(0);
+    textSize(20);
+    text("Press SHIFT for legend", 760, 50);
+  }
+  if (keyIsDown(SHIFT)) {
+    //legend
+    fill(0);
+    textSize(24);
+    text("1. Game Name\n2. Active Players\n3. Category\n4. Run Time", 800, 50);
+    legA = false;
+  } else {
+    legA = true;
   }
 }
